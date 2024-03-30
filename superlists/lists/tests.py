a7ba6856f6b05d91768b1df115ee6b43e0e728f4
Nima Soufiani <n_soufiani@hotmail.com>
2024-03-30 19:35:56 +0000

    move tests so they have single reposibility

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -8,12 +8,12 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
-        '''
+        """
         resolve is the function Django uses internally to resolve
         URLs and find what view function they should map to.  We're checking that
         resolve, when called with “/”, the root of the site, finds a function
         called home_page.
-        '''
+        """
 
         found = resolve("/")
         self.assertEqual(found.func, home_page)
@@ -21,13 +21,13 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         # request = HttpRequest()
         # response = home_page(request)
-        '''
+        """
         Instead of manually creating an HttpRequest object and calling the view
         function directly, we call self.client.get, passing it the URL we want
         to test.
-        '''
+        """
         response = self.client.get("/")
-        # below are old assertions
+        # below are old assertions
         # html = response.content.decode("utf8")
         # self.assertTrue(html.startswith("<html>"))
         # self.assertIn("<title>To-Do lists</title>", html)
@@ -38,21 +38,6 @@ class HomePageTest(TestCase):
     def test_only_saves_item_when_necessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_can_save_a_POST_request(self):
-        self.client.post("/", data={"item_text": "A new list item"})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redirects_after_POST(self):
-        response = self.client.post("/", data={"item_text": "A new list item"})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(
-            response["location"], "/lists/the-only-list-in-the-world"
-        )
 
 
 class ItemModelTest(TestCase):
@@ -80,15 +65,27 @@ class LiveViewTest(TestCase):
         Item.objects.create(text="item 2")
 
         response = self.client.get("/lists/the-only-list-in-the-world")
-        '''
+        """
             Here's a new helper method: instead of using the slightly annoying
             assertIn/response.content.decode() dance, Django provides the
             assertContains method, which knows how to deal with responses and
             the bytes of their content.
-        '''
+        """
         self.assertContains(response, "item 1")
         self.assertContains(response, "item 2")
 
     def test_uses_list_template(self):
         response = self.client.get("/lists/the-only-list-in-the-world")
         self.assertTemplateUsed(response, "list.html")
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/lists/new", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -20,7 +20,8 @@ from lists import views
 urlpatterns = [
     re_path(r"^$", views.home_page, name="home"),
     # re_path(r"^lists/the-only-list-in-the-world/$", views.view_list, name='view_list')
-    path('lists/the-only-list-in-the-world', views.view_list, name='view_list')
+    path('lists/new', views.new_list, name='new_list'),
+    path('lists/the-only-list-in-the-world', views.view_list, name='view_list'),
     # re_path(
     #     r"^lists/the-only-list-in-the-world/$",
     #     views.view_list,
