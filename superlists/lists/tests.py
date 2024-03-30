20b27615d801052ea4a86c291eb85c5def76a6e9
Nima Soufiani <n_soufiani@hotmail.com>
2024-03-30 17:06:22 +0000

    added lists view and simplified home template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -54,15 +54,6 @@ class HomePageTest(TestCase):
             response["location"], "/lists/the-only-list-in-the-world"
         )
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="item 1")
-        Item.objects.create(text="item 2")
-
-        response = self.client.get("/")
-
-        self.assertIn("item 1", response.content.decode())
-        self.assertIn("item 2", response.content.decode())
-
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -97,3 +88,7 @@ class LiveViewTest(TestCase):
         '''
         self.assertContains(response, "item 1")
         self.assertContains(response, "item 2")
+
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world")
+        self.assertTemplateUsed(response, "list.html")
