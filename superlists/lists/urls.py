5fadd92dac1def1da2086f71d61d40d48ebff21b
Nima Soufiani <n_soufiani@hotmail.com>
2024-03-30 22:06:53 +0000

    refactor urls

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
-from lists import urls as list_urls
-from lists import views as list_views
+from django.urls import path, re_path
+from lists import views
 
 urlpatterns = [
-    re_path(r"^$", list_views.home_page, name="home"),
-    re_path(r"^lists/", include(list_urls), name="")
+    re_path(r"^$", views.home_page, name="home"),
+    # re_path(r"^lists/the-only-list-in-the-world/$", views.view_list, name='view_list')
+    path('new', views.new_list, name='new_list'),
+    re_path(r'^(\d+)$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name="add_item")
 ]
